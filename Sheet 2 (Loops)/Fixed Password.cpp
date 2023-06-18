ba95a230e67567d0c26dab544e374331a352d9c5
Alzahraa Mohy <65045953+alzahraamohy@users.noreply.github.com>
2023-06-18 22:02:53 +0200

    Create Fixed Password.cpp

---
+++ Sheet 2 (Loops)/Fixed Password.cpp
@@ -0,0 +1,17 @@
+//Problem Link:https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/D
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int x;
+    while(x!=1999){
+        cin>>x;
+    if(x!=1999) cout<<"Wrong"<<endl;
+    else cout<<"Correct"<<endl;
+    
+    }
+ 
+    return 0;
+}
