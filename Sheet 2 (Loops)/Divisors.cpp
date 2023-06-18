2638ec8c1c6276c7785a4184b35371e28e699827
Alzahraa Mohy <65045953+alzahraamohy@users.noreply.github.com>
2023-06-18 22:10:38 +0200

    Create Divisors.cpp

---
+++ Sheet 2 (Loops)/Divisors.cpp
@@ -0,0 +1,17 @@
+//problem link : https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/K
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cin>>num;
+    
+    for(int i=1;i<=num;i++){
+        if(num%i==0)
+            cout<<i<<endl;
+    }
+
+    return 0;
+}
