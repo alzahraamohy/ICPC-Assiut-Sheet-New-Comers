c29644fb5800d76e8002405544a59df02c63fb1c
Alzahraa Mohy <65045953+alzahraamohy@users.noreply.github.com>
2023-06-18 22:04:52 +0200

    Create Multiplication table.cpp

---
+++ Sheet 2 (Loops)/Multiplication table.cpp
@@ -0,0 +1,13 @@
+//Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/F
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cin>>x;
+	for(int i=1;i<=12;i++){
+	    cout<<x<<" * "<<i<<" = "<<x*i<<endl;
+	    
+	}
+	return 0;
+}
