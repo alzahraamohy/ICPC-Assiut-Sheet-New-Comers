dd1205c7855531a64eef785a2937257993758a5e
Alzahraa Mohy <65045953+alzahraamohy@users.noreply.github.com>
2023-06-18 21:58:15 +0200

    Create 1 to N.cpp

---
+++ Sheet 2 (Loops)/1 to N.cpp
@@ -0,0 +1,15 @@
+//Problem link : https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/A
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{   int i,n;
+    cin>>n;
+    for (i=1;i<=n;i++){
+        
+        cout<<i<<endl;
+    }
+ 
+    return 0;
+}
