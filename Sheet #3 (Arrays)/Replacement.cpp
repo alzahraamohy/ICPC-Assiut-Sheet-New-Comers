7d8ef4eb560def8eaacac05222c417d2c1baa56b
Alzahraa Mohy <65045953+alzahraamohy@users.noreply.github.com>
2023-06-18 22:30:01 +0200

    Create  Replacement.cpp

---
+++ Sheet #3 (Arrays)/Replacement.cpp
@@ -0,0 +1,19 @@
+//PL: https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/C
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long x;
+    cin >> x;
+    long long arr[x];
+    for(int i=0; i<x; i++) {
+        cin >> arr[i];
+        if(arr[i]>0)
+            cout<<1<<" ";
+        else if(arr[i]<0)
+            cout<<2<<" ";
+        else if(arr[i]==0)
+            cout<<0<<" ";
+    }
+    return 0;
+}
